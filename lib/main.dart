317c6e4c197b6d7efacb0e4c4c0548164037d2fd
Kojo Syntax <albert.gbeve@stu.ucc.edu.gh>
2023-10-23 20:36:36 +0000

    Learnt scaffold and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Home Page"),
+        actions: [
+          IconButton(onPressed: (){
+
+          }, icon: Icon(Icons.notifications))
+        ],
+      ),
+    );
+  }
+}
